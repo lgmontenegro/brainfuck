fa1a7a0574daadb85a14ef81c92a0de5fefa00e2
Leonardo Montenegro <leonardo@montenegro.com.br>
2021-11-15 17:19:21 +0000

    Solving loop problems

---
+++ example/example.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"lgmontenegro/brainfuck"
+	"strings"
+)
+
+func main() {
+	teste := brainfuck.NewCompiler()
+
+	r := strings.NewReader(`++++++
+[
+>+++++++++++
+<-
+]
+>-.`)
+
+	bufferReadSize := make([]byte, 1)
+	for {
+		_, err := r.Read(bufferReadSize)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		err = teste.Compile(bufferReadSize[0])
+		if err != nil {
+			panic(err.Error())
+		}
+	}
+}
